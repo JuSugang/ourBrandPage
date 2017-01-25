9747b2f343c65895e59e7bebceba2768c3b7535c
JuSugang <tnrkd7679@naver.com>
2017-01-25 12:42:55 +0000

    our first setting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OurBrandPage::Application.config.secret_key_base = 'e3e1e471158ba99a74a4b0e5a6c568b78f72d71ef83233cc09f5ea19943af37f36f3a8a01152ce39200b203f94b3a35fedc3d81c3c5a4a6dde1bd89114d3bd1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OurBrandPage::Application.config.session_store :cookie_store, key: '_ourBrandPage_session'
